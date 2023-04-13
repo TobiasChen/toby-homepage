def33ee8d8696f0b479ae6e1f87c9825676f8522
Tobias Chen <tobias@chen.de>
2023-04-13 18:16:36 +0200

    Initial Setup

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <div>
+      </div>
+        <h1 className="text-3xl font-bold underline">
+            Hello world!
+        </h1>
+      <h1>Vite + React</h1>
+      <div className="card bg-red">
+        <button className="bg-blue" onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default App
