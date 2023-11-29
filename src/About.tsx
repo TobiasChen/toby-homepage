7c794e82b998519d60e78e90c5e5e536e6beb614
Tobias Chen <tobias@chen.de>
2023-11-29 16:34:00 +0100

    Updated Previous Work Experience

--- src/About.tsx
+++ src/About.tsx
@@ -9,18 +9,16 @@ export default function About(prop: { title: string }) {
 
   return (
     <div className="main flex flex-col justify-center gap-4 grow max-w-5xl ml-12 mr-12">
-      <p className=" text-5xl text-center mb-5 font-normal">Hey hey :D</p>
-      <p className=" text-xl text-center mb-10">My name's Tobias Chen(more commenly Toby). I am a full-stack developer with a Bsc. in computer science and a strong interest in developer operations.</p>
-      <p className=" text-xl text-center">Im currently a</p>
-      <div className="text-center">    
-        <ul className="text-xl text-left inline-block list-disc">
-            <li>Fullstack Developer @ <a href="www.seitenbau.com" rel="noopener noreferrer" target="_blank">Seitenbau</a></li>
-        </ul>
-      </div>
+      <p className=" text-5xl text-center mb-5 font-normal">About me</p>
+      <p className=" text-xl text-center mb-10">My name's Tobias Chen, more commonly known as Toby. I am a full-stack developer with a Bsc. in computer science and
+       a strong passion for development, covering a wide range of areas including both back- and frontend, developer operations, and cloud technologies.</p>
+      <p className=" text-xl text-center mb-10">If your interested in working with me, head over to my <Link to={"/contact"}>contact</Link> page</p>
       <p className=" text-xl text-center">I've previously been:</p>
       <div className="text-center">    
         <ul className="text-xl text-left inline-block list-disc">
-            <li>Junior DevOps Enginer @ <a href="www.valtech-mobility.de" rel="noopener noreferrer" target="_blank">Valtech Mobility</a></li>
+            <li>DevOps Engineer @ <a href="www.datatactics.de" rel="noopener noreferrer" target="_blank">Datatactics</a></li>
+            <li>Fullstack Developer @ <a href="www.seitenbau.com" rel="noopener noreferrer" target="_blank">Seitenbau</a></li>
+            <li>Junior DevOps Engineer @ <a href="www.valtech-mobility.de" rel="noopener noreferrer" target="_blank">Valtech Mobility</a></li>
             <li>Junior Software Developer @ <a href="www.ida-sds.com" rel="noopener noreferrer" target="_blank">Intelligent Data Analytics</a></li>
         </ul>
       </div>
