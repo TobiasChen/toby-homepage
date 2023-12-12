63b47ac95e80090e0555ffb7fe5a745dbcfa41d8
Tobias Chen <tobias@chen.de>
2023-12-12 09:56:32 +0100

    Updated About.tsx

--- src/About.tsx
+++ src/About.tsx
@@ -13,10 +13,15 @@ export default function About(prop: { title: string }) {
       <p className=" text-xl text-center mb-10">My name's Tobias Chen, more commonly known as Toby. I am a full-stack developer with a Bsc. in computer science, experienced in a wide range of areas including both back- and frontend. I have
        a strong passion for development,  developer operations, and cloud technologies.</p>
       <p className=" text-xl text-center mb-10">If your interested in working with me, head over to my <Link to={"/contact"}>contact</Link> page</p>
+      <p className=" text-xl text-center">Im currently a</p>
+      <div className="text-center">    
+        <ul className="text-xl text-left inline-block list-disc">
+          <li>DevOps Engineer @ <a href="www.datatactics.de" rel="noopener noreferrer" target="_blank">Datatactics</a></li>
+        </ul>
+      </div>
       <p className=" text-xl text-center">I've previously been:</p>
       <div className="text-center">    
         <ul className="text-xl text-left inline-block list-disc">
-            <li>DevOps Engineer @ <a href="www.datatactics.de" rel="noopener noreferrer" target="_blank">Datatactics</a></li>
             <li>Fullstack Developer @ <a href="www.seitenbau.com" rel="noopener noreferrer" target="_blank">Seitenbau</a></li>
             <li>Junior DevOps Engineer @ <a href="www.valtech-mobility.de" rel="noopener noreferrer" target="_blank">Valtech Mobility</a></li>
             <li>Junior Software Developer @ <a href="www.ida-sds.com" rel="noopener noreferrer" target="_blank">Intelligent Data Analytics</a></li>
